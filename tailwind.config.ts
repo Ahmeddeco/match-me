4fd841c21fcfb08c4e4a90526adfa3e170adfaba
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-12-13 01:05:20 +0200

    start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/theme'
+import type { Config } from "tailwindcss"
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js"
   ],
   theme: {
     extend: {
@@ -14,5 +16,5 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [nextui()],
+} satisfies Config
